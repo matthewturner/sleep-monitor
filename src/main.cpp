8ed1b8b0720f8542275e4b885d99bf0b1e155c2f
Matt Turner <matt@fiverings.uk>
2021-02-27 12:06:24 +0000

    Add detection for start and end of sound

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define SOUND_DETECTED LOW
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(7, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+bool soundStarted = false;
+unsigned long lastSound = 0;
+unsigned long currentSilence = 0;
+
+void loop()
+{
+  int value = digitalRead(7);
+
+  if (value == SOUND_DETECTED)
+  {
+    lastSound = millis();
+    if (soundStarted)
+    {
+      // sound continuing
+    }
+    else
+    {
+      Serial.println("Sound started...");
+      soundStarted = true;
+    }
+  }
+  else
+  {
+    if (soundStarted)
+    {
+      currentSilence = millis();
+      if (currentSilence - lastSound > 300)
+      {
+        Serial.println("Sound finished...");
+        soundStarted = false;
+      }
+    }
+    else
+    {
+      // silence continuing...
+    }
+  }
 }
